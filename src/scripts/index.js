6f0e9c2ff122bfa7515603e6396d922eead1796d
rvDOOM <rvalerio9131@gmail.com>
2023-11-23 17:09:54 -0500

    Added footer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,6 @@
 const headerContainer = document.querySelector('.header-display');
+const contentContainer = document.querySelector('.content-display');
+const footerContainer = document.querySelector('.footer-display')
 
 fetch('../src/header.html')
   .then(response => response.text())
@@ -6,3 +8,14 @@ fetch('../src/header.html')
     headerContainer.innerHTML = data;
   });
 
+fetch('../src/home.html')
+  .then(response => response.text())
+  .then(data => {
+    contentContainer.innerHTML = data;
+  });
+
+fetch('../src/footer.html')
+  .then(response => response.text())
+  .then(data => {
+    footerContainer.innerHTML = data;
+  });
